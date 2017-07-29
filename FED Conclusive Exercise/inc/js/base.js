e488007409b672ca0ceec4c335f4285b1b818ce5
Zhuo Chen <zhuo.h.chen@gmail.com>
2017-07-28 20:40:12 -0400

    Remove comments and debuggers. Prep for submit

--- FED Conclusive Exercise/inc/js/base.js
+++ FED Conclusive Exercise/inc/js/base.js
@@ -42,29 +42,17 @@
 			const last = this.props.total_slides.length - 1;
 			this.props.current_slide = prev === last ? 0 : prev + 1;
 			this.update(prev, "next");
-			// const $current = $(".slide-text").first().parent();
-			// $(".slide-text").first().parent().remove();
-			// $(".carousel-cells").append($current[0]);
-			// this.update();
 		},
 		previous:function(){
 			const prev = this.props.current_slide;
 			const first = this.props.total_slides.length - 1;
 			this.props.current_slide = prev === 0 ? first : prev - 1;
 			this.update(prev, "previous");
-			// const $current = $(".slide-text").last().parent();
-			// $(".slide-text").last().parent().remove();
-			// $(".slide-text").first().parent().before($current);
-			// debugger
 		},
 		update:function(prev, type){
-			// debugger
 			$(".slide-text").addClass("hidden");
 			$(".slide-text").slice(prev, prev + 1).attr("class", "slide-text " + type);
 			$(".slide-text").slice(this.props.current_slide, this.props.current_slide + 1).attr("class", "slide-text current-" + type);
-
-			// this.props.total_slides.addClass("hidden");
-			// $(this.props.total_slides[this.props.current_slide]).attr("class", "slide-text");
 		}
 	}
 	$(function(){
