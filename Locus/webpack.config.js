8abb4acb953ddfe10e54dfa4f017a29a7723ea08
Zhuo Chen <zhuo.h.chen@gmail.com>
2017-10-06 16:44:58 -0400

    Start Locus. Init npm, webpack, babel.

---
+++ Locus/webpack.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  entry: './lib/root.js',
+  output: {
+    path: path.resolve(__dirname, 'lib'),
+    filename: './lib/bundle.js',
+  },
+  module: {
+    loaders: [
+      {
+        test: [/\.jsx?$/],
+        exclude: /(node_modules)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015']
+        }
+      }
+    ]
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['.js', '*']
+  },
+};
