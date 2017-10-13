be71e0b89f824bb1d4594c6a5307724cabb3b28a
Zhuo Chen <zhuo.h.chen@gmail.com>
2017-10-12 20:12:54 -0400

    Add custom chords generator. Rendering chords.

--- Locus/lib/chart.js
+++ Locus/lib/chart.js
@@ -1,5 +1,6 @@
 import * as d3 from 'd3';
 import * as Util from './util.js';
+import { merge } from 'lodash';
 
 class Chart {
   constructor(props) {
@@ -8,6 +9,10 @@ class Chart {
     this.x = 0;
     this.y = 0;
     this.getPie = this.getPie.bind(this);
+    this.get
+    this.sources = {};
+    this.targets = {};
+    this.counter = 0;
   }
 
   initialize() {
@@ -48,32 +53,52 @@ class Chart {
     this.g = this.svg
       .append('g')
       .attr('transform', 'translate(' + (this.renderDims[0] / 2) + ',' + (this.renderDims[1] / 2) + ')')
-      .datum(this.getChord()(this.matrix));
+      // .datum(this.getChord()(this.matrix));
   }
+
+  getKey(d) {
+    return Object.keys(d)[0];
+  }
+
+//   getRibbon(node) {
+//     const start = node.startAngle;
+//     const end = node.endAngle;
+//     const radius = this.radius - 70;
+//     let ribbon = d3.ribbon()
+//       .radius(radius)
+//       .source({ startAngle: start, endAngle: start, radius })
+//       .target({ startAngle: end, endAngle: end, radius });
+// debugger
+//     // ribbon({
+//     //   source: { startAngle: start, endAngle: start, radius },
+//     //   target: { startAngle: end, endAngle: end, radius },
+//     // });
+//
+//     return ribbon;
+//   }
 
   getRibbon() {
     return d3.ribbon()
       .radius(this.radius - 70);
   }
+  // setGroup() {
+  //   this.group = this.g
+  //     .append('g')
+  //     .attr('class', 'groups')
+  // }
 
-  setGroup() {
-    this.group = this.g
-      .append('g')
-      .attr('class', 'groups')
-  }
-
-  getGroup() {
-    this.group
-      .selectAll('donut')
-      .data(d => this.getPie()(d.groups))
-      .enter()
-      .append('path')
-      .attr('d', this.getArc())
-      .attr('class', 'donut')
-      .attr('id', (d, i) => 'pie' + i.toString())
-      .style('fill', (d, i)=> this.colors(i))
-      .style('stroke', (d, i) => this.colors(i))
-  }
+  // getGroup() {
+  //   this.group
+  //     .selectAll('donut')
+  //     .data(d => this.getPie()(d.groups))
+  //     .enter()
+  //     .append('path')
+  //     .attr('d', d => this.getArc(d))
+  //     .attr('class', 'donut')
+  //     .attr('id', (d, i) => 'pie' + i.toString())
+  //     .style('fill', (d, i)=> this.colors(i))
+  //     .style('stroke', (d, i) => this.colors(i))
+  // }
 
   getArc() {
     return d3.arc()
@@ -83,38 +108,75 @@ class Chart {
 
   getPie() {
     return d3.pie()
-      .value(d => d.value)
+      .value((d, i) => {
+        const key = Object.keys(d)[0];
+        const item = d[key];
+        const parents = item.parents.length > 0 ? item.parents.length : 1;
+        const children = item.children.length > 0 ? item.children.length : 1;
+
+        return parents + children;
+      })
       .sort(null);
   }
-  // getPie() {
-  //   return d3.pie()
-  //     .value((d, i) => {
-  //       const key = Object.keys(d)[0];
-  //       const item = d[key];
-  //       const parents = item.parents.length > 0 ? item.parents.length : 1;
-  //       const children = item.children.length > 0 ? item.children.length : 1;
-  //
-  //       return parents + children;
-  //     })
-  //     .sort(null);
-  // }
 
-  getChord() {
+  getChord(d, i) {
     return d3.chord()
       .padAngle(0.5);
   }
 
-  getRibbons() {
-    this.g
-      .append('g')
-      .attr('class', 'ribbons')
-      .selectAll('path')
-      .data(chords => chords)
-        .enter()
-        .append('path')
-        .attr('d', this.getRibbon())
-        .style('fill', (d, i) => this.colors(i))
-        .style('stroke', (d, i) => this.colors(i));
+  getRibbons(d, i) {
+    const data = d.data[this.getKey(d.data)];
+    const children = data.children;
+    const parents = data.parents;
+    const angle = (d.startAngle + d.endAngle) / 2;
+
+    children.forEach(child => this.genChildRibbon(child, angle));
+    parents.forEach(parent => this.genParentRibbon(parent, angle));
+  }
+
+  genChildRibbon(child, angle) {
+    child.endAngle = angle;
+    const key = child.Source;
+    const target = this.targets[child.Source];
+
+    if (target !== undefined) {
+      const source = target.find(el => el.Source === key);
+      if (source !== undefined) {
+        this.createRibbon(source);
+      } else {
+      }
+    } else {
+      // this.sources[key] = [child];
+    }
+    this.sources[key] = this.sources[key] !== undefined ? this.sources[key].concat([child]) : [child];
+  }
+
+  genParentRibbon(parent, angle) {
+    parent.startAngle = angle;
+    const key = parent.Target;
+    const source = this.sources[parent.Source];
+    this.targets[key] = this.targets[key] !== undefined ? this.targets[key].concat([parent]) : [parent];
+
+    if (source !== undefined) {
+      const target = source.find(el => el.Target === key);
+
+      if (target !== undefined) {
+        this.createRibbon(target);
+      } else {
+      }
+    } else {
+      // this.targets[key] = [parent] ;
+    }
+  }
+
+  createRibbon(node) {
+    this.counter += 1;
+    // this.g
+    // .selectAll('g')
+    // .append('path')
+    // .attr('d', this.getRibbon(node))
+    // .style('fill', (d, i) => this.colors(i))
+    // .style('stroke', (d, i) => this.colors(i));
   }
 
   getChart() {
@@ -122,19 +184,102 @@ class Chart {
       .attr('class', 'chart')
       .selectAll('donut')
       .data(this.getPie()(this.data))
-      .enter()
-      .append('path')
-      .attr('d', this.getArc())
-      .attr('class', 'donut')
-      .attr('id', (d, i) => 'pie' + Object.keys(d.data)[0].toString())
-      .attr('fill', (d, i) => this.colors(Object.keys(d.data)[0]));
+        .enter()
+        .append('path')
+        .attr('d', this.getArc())
+        .attr('class', 'donut')
+        .attr('id', (d, i) => {
+          this.getRibbons(d, i)
+          'pie' + Object.keys(d.data)[0].toString()
+        })
+        .attr('fill', (d, i) => this.colors(Object.keys(d.data)[0]))
+          // .append('path')
+          // .attr('d', (d, i) => this.getRibbons(d,i))
+          // .attr('class', 'ribbon')
+          // .attr('fill', (d, i) => this.colors(Object.keys(d.data)[0]))
+
+  }
+
+  createChord() {
+    const width = this.renderDims[0], height = this.renderDims[1];
+    const colors = this.colors;
+
+    this.g.append("g")
+      .attr('class', 'chords')
+      .selectAll('ribbon')
+        .data(this.chords)
+        .enter()
+        .append("path")
+        .attr("d", this.getRibbon())
+        .attr('class', 'ribbon')
+        .attr("fill", (d, i) => this.colors(i))
+        .style('stroke', (d, i) => this.colors(i))
+        .style('stroke-width', (d, i) => {
+          debugger
+        })
+        // .style("stroke", (d, i) => this.colors[i]);
+      // .append("g")
+      // .attr("class", "ribbons")
+      // .selectAll("path")
+// debugger
+    // var group = g.append("g")
+    //     .attr("class", "groups")
+    //   .selectAll("g")
+    //   .data(function(chords) { return chords.groups; })
+    //   .enter().append("g");
+    //
+    // group.append("path")
+    //     .style("fill", (d, i) => this.colors[i])
+    //     .style("stroke", (d, i) => this.colors[i])
+    //     .attr("d", this.getArc());
+
+      }
+
+  genChords() {
+    const chord = [];
+    const targets = this.targets;
+
+    for (let prop in targets) {
+      const target = targets[prop];
+      for (let i = 0; i < target.length; i++) {
+        const item = target[i];
+        const chordItem = {
+          source: {
+            index: item.Source,
+            subindex: item.Target,
+            startAngle: item.startAngle,
+            endAngle: item.startAngle,
+            value: 10,
+          },
+          target: {
+            index: item.Target,
+            subindex: item.Source,
+            startAngle: item.endAngle,
+            endAngle: item.endAngle,
+          }
+        }
+
+        chord.push(chordItem);
+      }
+    }
+
+    chord.group = {};
+    this.chords = chord;
   }
 
   render() {
-    // this.getChart();
-    this.getRibbons();
-    this.setGroup();
-    this.getGroup();
+    // const x = d3.chord()(this.matrix);
+    // debugger
+    this.getChart();
+    // this.getRibbons();
+    // this.setGroup();
+    // this.getGroup();
+
+    // const total = Object.keys(this.targets).map(key => this.targets[key].length).reduce((accum, num) => accum + num);
+    // const result = Object.keys(this.sources).map(key => this.sources[key].length).reduce((accum, num) => accum + num);
+    this.genChords();
+    this.createChord();
+    // debugger
   }
 }
 
